80822cf808956e89fc74f7292ac1d7521d20c081
WilcoApp <hello@wilco.gg>
2025-12-17 20:25:59 +0000

    Initial commit

--- chain_prompt.js
+++
@@ -1,9 +0,0 @@
-function toKebabCase(str) {
-    return str
-        .replace(/[\s_]+/g, '-')
-        .replace(/[^\w-]/g, '')
-        .toLowerCase();
-}
-
-console.log(toKebabCase("Hello World_plays"))
-
